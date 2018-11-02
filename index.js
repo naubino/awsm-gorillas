217ffb968ec5cfc6809d7d50f2849eb278951600
Hendrik Sollich <hendrik@hoodie.de>
2018-11-02 21:55:26 +0100

    jigsaw falling into place

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ async function main() {
     const js = await import("./pkg");
 
     const canvas = document.createElement('canvas');
+    const width = 800;
+    const height = 800;
+    canvas.width = width;
+    canvas.height = height;
     document.body.appendChild(canvas);
-    const game = new js.Game(canvas);
+    const game = new js.Game(canvas, {width, height});
     game.setup_boxes_scene();
     const loop = () => {
         game.step();
         game.render_scene();
-        //requestAnimationFrame(loop);
+        requestAnimationFrame(loop);
     }
     loop();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,11 @@ extern crate nalgebra as na;
 extern crate ncollide2d;
 extern crate nphysics2d;
 
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 use na::{Vector2};
 use ncollide2d::shape::{Cuboid};
 use ncollide2d::world::CollisionObjectHandle;
@@ -21,7 +26,21 @@ type World = nphysics2d::world::World<f64>;
 type Isometry2 = na::Isometry2<f64>;
 type ShapeHandle = ncollide2d::shape::ShapeHandle<f64>;
 
-//
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(msg: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn debug(msg: &str);
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GameConfig {
+    width: Option<f64>,
+    height: Option<f64>,
+}
 
 #[wasm_bindgen]
 pub struct Game {
@@ -29,11 +48,12 @@ pub struct Game {
     world: World,
 }
 
-
 #[wasm_bindgen]
 impl Game {
     #[wasm_bindgen(constructor)]
-    pub fn new(canvas: HtmlCanvasElement) -> Game {
+    pub fn new(canvas: HtmlCanvasElement, config: &JsValue) -> Game {
+        let conf: GameConfig = config.into_serde().unwrap();
+        debug(&format!("game config: {:?}", conf));
         Game {
             canvas: canvas,
             world: World::new(),
@@ -46,53 +66,13 @@ impl Game {
 
     pub fn render_scene(&self) {
         let context = canvas_get_context_2d(&self.canvas);
+        context.clear_rect(0., 0., self.canvas.width().into(), self.canvas.height().into());
         render_nphysics_world(&self.world, context);
     }
 
     pub fn step(&mut self) {
         self.world.step();
     }
-}
-
-fn canvas_get_context_2d(canvas: &HtmlCanvasElement) -> CanvasRenderingContext2d {
-    canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()
-        .unwrap()
-}
-
-fn render_nphysics_world(world: &World, ctx: CanvasRenderingContext2d) {
-    world.colliders().for_each(|collider| {
-        if let Some(body) = world.rigid_body(collider.data().body()) {
-            let pos = body.position().translation.vector;
-            let shape = collider.shape();
-            if shape.is_shape::<Cuboid<_>>() {
-                ctx.begin_path();
-                ctx.rect(20.0 + pos.x * 100.0, pos.y * 100.0, 10.0, 10.0);
-                ctx.fill();
-                // console::log_2(&pos.x.into(), &pos.y.into());
-            }
-        }
-    });
-}
-
-fn make_ground(world: &mut World) -> CollisionObjectHandle {
-    let margin = 0.01;
-    let radius_x = 25.0 - margin;
-    let radius_y = 1.0 - margin;
-    let radius = Vector2::new(radius_x, radius_y);
-    let cuboid = Cuboid::new(radius);
-    let shape = ShapeHandle::new(cuboid);
-    let pos = Isometry2::new(-Vector2::y() * radius_y, na::zero());
-    world.add_collider(
-        margin,
-        shape,
-        BodyHandle::ground(),
-        pos,
-        Material::default(),
-    )
 }
 
 struct SimpleBox {
@@ -110,12 +90,40 @@ impl SimpleBox {
     }
 }
 
+fn canvas_get_context_2d(canvas: &HtmlCanvasElement) -> CanvasRenderingContext2d {
+    canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap()
+}
+
+fn make_ground(world: &mut World) -> CollisionObjectHandle {
+    let margin = 0.01;
+    let radius_x = 125.0 - margin;
+    let radius_y = 1.0 - margin;
+    let radius = Vector2::new(radius_x, radius_y);
+    let cuboid = Cuboid::new(radius);
+    let shape = ShapeHandle::new(cuboid);
+    let pos = Isometry2::new(Vector2::new(0., 8.), na::zero());
+    debug(&format!("make_ground {:?}", (margin, radius_x, radius_y, radius, pos)));
+    world.add_collider(
+        margin,
+        shape,
+        BodyHandle::ground(),
+        pos,
+        Material::default(),
+    )
+}
+
+
 fn make_box_shape(radx: f64, rady: f64) -> ShapeHandle {
     ShapeHandle::new(Cuboid::new(Vector2::new(radx, rady)))
 }
 
 fn make_simple_body(world: &mut World, transform: Isometry2, shape: ShapeHandle) -> BodyHandle {
-    world.add_rigid_body(transform, shape.inertia(1.0), shape.center_of_mass())
+    world.add_rigid_body(transform, shape.inertia(0.1), shape.center_of_mass())
 }
 
 fn make_simple_collider(world: &mut World, shape: ShapeHandle, body: BodyHandle) -> CollisionObjectHandle {
@@ -128,15 +136,15 @@ fn make_simple_collider(world: &mut World, shape: ShapeHandle, body: BodyHandle)
 // example nphysics scenes
 
 fn setup_nphysics_boxes_scene(world: &mut World) {
-    world.set_gravity(Vector2::new(0.0, -9.81));
-    
+    world.set_gravity(Vector2::new(0.0, 9.81));
+
     let ground = make_ground(world);
 
     let num = 25;
     let radx = 0.1;
     let rady = 0.1;
     let shiftx = radx * 2.0;
-    let shifty = rady * 2.0;
+    let shifty = rady * 3.0;
     let centerx = shiftx * num as f64 / 2.0;
     let centery = shifty / 2.0;
 
@@ -149,3 +157,33 @@ fn setup_nphysics_boxes_scene(world: &mut World) {
         }
     }
 }
+
+fn render_nphysics_world(world: &World, ctx: CanvasRenderingContext2d) {
+    world.colliders().for_each(|collider| {
+        if let Some(body) = world.rigid_body(collider.data().body()) {
+
+            let pos = body.position().translation.vector;
+            let x = pos.x;
+            let y = pos.y;
+
+            let rotation = body.position().rotation;
+            let shape = collider.shape();
+
+            if let Some(cube) = shape.as_shape::<Cuboid<_>>() {
+                let size = cube.half_extents();
+                let (w, h) = (size.x, size.y);
+                ctx.begin_path();
+                ctx.save();
+                ctx.scale(100., 100.);
+                ctx.translate(x-w, y-h);
+                ctx.rotate(rotation.angle());
+                ctx.rect(0.,0., w*2., h*2.);
+                // ctx.rect(20.0 + pos.x * 100.0, pos.y * 100.0, 10.0, 10.0);
+                ctx.fill();
+                ctx.restore();
+                // console::log_2(&pos.x.into(), &pos.y.into());
+            }
+        }
+    });
+}
+
