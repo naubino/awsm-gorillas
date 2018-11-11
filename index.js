bb519f809f4cdc17b13aff9efd37231fcec2df3f
Hendrik Sollich <hendrik@hoodie.de>
2018-11-11 01:12:35 +0100

    invert shoot logic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initGamePad, gamepadNormalize, controllers, scangamepads } from './game
 
 const DEBUG_GAME_PAD = !true;
 
-window.viewConfig = {x: 0, y: 0, rotation: 0, zoom: 50.0};
+window.viewConfig = {x: 0, y: 0.6, rotation: 0, zoom: 50.0};
 
 void async function main() {
     const wasm = await import("./pkg");
@@ -66,29 +66,19 @@ void async function main() {
             {x:  3.0, w: 3, h: 15, fill_style: style[4] },
             {x:  4.6, w: 3, h: 12, fill_style: style[1] },
             {x:  7.0, w: 5, h: 21, fill_style: style[0] },
-
-
         ],
 
         player_a: {x: -7.0, y: -1.0, radx: 0.2, rady: 0.3, inertia: 0.1 },
         player_b: {x:  7.0, y: -1.0, radx: 0.2, rady: 0.3, inertia: 0.1 },
     });
 
-    const keyboard = {
-        axes : [0,0,0,0],
-        buttons : [
-            {value : 0, pressed : false},
-            {value : 0, pressed : false},
-            {value : 0, pressed : false},
-            {value : 0, pressed : false}]
-    };
-
     let last_time = + performance.now();
+
     const loop = (timestamp) => {
         try { scangamepads(); } catch {}
 
-        if (controllers[0]) controlPlayer(0, controllers[0]);
-        if (controllers[1]) controlPlayer(1, controllers[1]);
+        if (controllers[0]) controlPlayer(0, controllers[0], (btn) => shoot({game, playerIndex: 0}, btn));
+        if (controllers[1]) controlPlayer(1, controllers[1], (btn) => shoot({game, playerIndex: 1}, btn));
 
         const dt = +timestamp - last_time;
         last_time = +timestamp;
@@ -99,71 +89,75 @@ void async function main() {
         requestAnimationFrame(loop);
     }
     loop();
+}()
 
 
-    function controlPlayer(player, gamePad) {
-        const input = gamepadNormalize(gamePad);
+function controlPlayer(playerIndex, gamePad, shootCallback) {
+    const input = gamepadNormalize(gamePad);
 
-        const [ hori1, vert1, l2, hori2, vert2, r2, hori3, vert3 ] = input;
+    const [ hori1, vert1, l2, hori2, vert2, r2, hori3, vert3 ] = input;
 
-        const pos = game.gorilla_pos(player);
-        const mag = Math.sqrt(vert1*vert1 + hori1*hori1);
-        const rot = vert1 || hori1 ? Math.atan2(vert1, hori1) : 0;
+    const rot = vert1 || hori1 ? Math.atan2(vert1, hori1) : 0;
 
-        const btnX = gamePad.buttons[0].pressed;
-        const btnO = gamePad.buttons[1].pressed;
-        const btnTri = gamePad.buttons[2].pressed;
-        const btnSqr = gamePad.buttons[3].pressed;
-        const l1 = gamePad.buttons[4].pressed;
-        const r1 = gamePad.buttons[5].pressed;
+    const btnX = gamePad.buttons[0].pressed;
+    const btnO = gamePad.buttons[1].pressed;
+    const btnTri = gamePad.buttons[2].pressed;
+    const btnSqr = gamePad.buttons[3].pressed;
+    const l1 = gamePad.buttons[4].pressed;
+    const r1 = gamePad.buttons[5].pressed;
 
-        
-        const x = pos.x + hori1 * 0.2;
-        const y = pos.y + vert1 * 0.2;
-        const power = Math.max(8, 14 * mag);
+    
+    window.viewConfig.x -= hori2 * 0.5;
+    window.viewConfig.y -= vert2 * 0.5;
+    window.viewConfig.zoom += 0.3 * (-l2 + r2);
 
-        window.viewConfig.x -= hori2 * 0.5;
-        window.viewConfig.y -= vert2 * 0.5;
-        window.viewConfig.zoom += 0.3 * (-l2 + r2);
+    if (playerIndex === 0) {
+        // if (l1) {window.viewConfig.rotation += 0.05}
+        // if (r1) {window.viewConfig.rotation -= 0.05}
+        if (l1) { window.viewConfig.x += 0.5 }
+        if (r1) { window.viewConfig.x -= 0.5 }
+    }
+    shootCallback({btnX, btnO, btnTri, btnSqr, rot, hori1, vert1})
+}
 
-        if (l1) {window.viewConfig.rotation += 0.05}
-        if (r1) {window.viewConfig.rotation -= 0.05}
-        // if (l1) {window.viewConfig.x += 0.5}
-        // if (r1) {window.viewConfig.x -= 0.5}
+function shoot({game, playerIndex}, {btnX, btnO, btnSqr, btnTri, rot, hori1, vert1}) {
 
+    const pos = game.gorilla_pos(playerIndex);
+    const mag = Math.sqrt(vert1*vert1 + hori1*hori1);
+    const power = Math.max(8, 14 * mag);
+    const x = pos.x + hori1 * 0.2;
+    const y = pos.y + vert1 * 0.2;
 
-        let shot = { x, y, rot, power, gorilla_id: player };
-        if (btnX) {
-            shot.config = {
-                w: 0.2,
-                h: 0.08,
-                inertia: 0.1,
-                ttl: 20,
-                cost: 0.03,
-            };
+    let shot = { x, y, rot, power, gorilla_id: playerIndex };
+    if (btnX) {
+        shot.config = {
+            w: 0.2,
+            h: 0.08,
+            inertia: 0.1,
+            ttl: 20,
+            cost: 0.03,
+        };
+    }
+    if (btnO) {
+        shot.config = {
+            w: 0.3,
+            h: 0.1,
+            inertia: 1,
+            ttl: 20,
+            cost: 0.3,
         }
-        if (btnO) {
-            shot.config = {
-                w: 0.3,
-                h: 0.1,
-                inertia: 1,
-                ttl: 20,
-                cost: 0.3,
-            }
-        }
-        if (btnTri) {
-            shot.config = {
-                w: 1.0,
-                h: 0.3,
-                inertia: 3,
-                ttl: 10,
-                cost: 3.0,
-            }
-        }
-
-        if (btnX || btnO || btnTri) {
-            game.shoot(shot, (Math.random() - 0.5) * 100);
+    }
+    if (btnTri) {
+        shot.config = {
+            w: 1.0,
+            h: 0.3,
+            inertia: 3,
+            ttl: 10,
+            cost: 3.0,
         }
     }
 
-}()
+    if (btnX || btnO || btnTri) {
+        game.shoot(shot, (Math.random() - 0.5) * 100);
+    }
+}
